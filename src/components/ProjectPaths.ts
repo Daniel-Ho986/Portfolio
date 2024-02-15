c5844228fa9f3c3f5c3bb96f2a837fbf773cab1a
Daniel <dhonaruto@gmail.com>
2024-02-14 21:35:08 -0500

    implemented glassmorphism for project cards and adjust about section button to be in middle for smaller screen size

--- src/components/ProjectPaths.ts
+++ src/components/ProjectPaths.ts
@@ -1,10 +1,10 @@
 export const projects = [
 	{
-		title: "Portfolio",
-		description: "This is the my first project",
+		title: "My Portfolio",
+		description: "Explore my professional journey, skills, and projects in one place with my portfolio.",
 		image: "../../assets/stock-1.jpg",
-		code: "https://github.com/SDemero/capstone-client",
-		site: "https://ttp-capstone-investedfitness.netlify.app/",
+		code: "https://github.com/Daniel-Ho986/Portfolio",
+		site: "https://daniel-ho986-portfolio.netlify.app/",
 		techStack: [
             { path: "astro.svg", alt: "Astro", modeClasses: ["light-mode-only"] },
             { path: "astro-dark.svg", alt: "Astro", modeClasses: ["dark-mode-only"] },
@@ -14,11 +14,11 @@ export const projects = [
 		]
 	},
 	{
-		title: "Mini Project Gallery",
-		description: "This is the my first project",
-		image: "../../assets/stock-2.jpg",
-		code: "https://github.com/Daniel-Ho986/50-mini-projects",
-		site: "https://daniel-ho986-mini-projects.netlify.app/",
+		title: "Mini Project Showcase",
+		description: "Discover a collection of creative mini-projects showcasing HTML, CSS, and JavaScript skills.",
+		image: "../../assets/mini-project-showcase.png",
+		code: "https://github.com/Daniel-Ho986/mini-project-showcase",
+		site: "https://daniel-ho986-mini-project-showcase.netlify.app/",
 		techStack: [
 			{ path: "html5.svg", alt: "HTML5", modeClasses: [] },
 			{ path: "css3.svg", alt: "CSS3", modeClasses: [] },
