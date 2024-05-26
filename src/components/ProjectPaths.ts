0b8f35a38a09690c74a4c585a888fd7b2b0695a6
Daniel <dhonaruto@gmail.com>
2024-05-26 14:49:39 -0400

    restructured stack-icons folder

--- src/components/ProjectPaths.ts
+++ src/components/ProjectPaths.ts
@@ -2,21 +2,36 @@ export const projects = [
 	{
 		title: "My Portfolio",
 		description: "Explore my professional journey, skills, and projects in one place with my portfolio.",
-		image: "../../assets/portfolio.png",
+		image: "../../assets/project-images/portfolio.png",
 		code: "https://github.com/Daniel-Ho986/Portfolio",
 		site: "https://daniel-ho986-portfolio.netlify.app/",
 		techStack: [
             { path: "astro.svg", alt: "Astro", modeClasses: ["light-mode-only"] },
             { path: "astro-dark.svg", alt: "Astro", modeClasses: ["dark-mode-only"] },
+			{ path: "javascript.svg", alt: "JavaScript", modeClasses: [] },	
 			{ path: "typescript.svg", alt: "TypeScript", modeClasses: [] },
 			{ path: "css3.svg", alt: "CSS3", modeClasses: [] },
-			{ path: "javascript.svg", alt: "JavaScript", modeClasses: [] },	
+		]
+	},
+	{
+		title: "Campsite",
+		"description": "Discover and share the best camping spots with Campsite. Browse, create, and review campgrounds with full CRUD functionality, interactive maps, image uploads, and user authentication.",
+		image: "../../assets/project-images/campsite.png",
+		code: "https://github.com/Daniel-Ho986/campsite",
+		site: "https://campsite-5u82.onrender.com/",
+		techStack: [
+			{ "path": "nodejs.svg", "alt": "Node.js", "modeClasses": [] },
+			{ "path": "express.svg", "alt": "Express", "modeClasses": [] },
+			{ "path": "mongodb.svg", "alt": "MongoDB", "modeClasses": [] },
+			// { "path": "mongoose.svg", "alt": "Mongoose", "modeClasses": [] },
+			// { "path": "cloudinary.svg", "alt": "Cloudinary", "modeClasses": [] },
+			// { "path": "bootstrap.svg", "alt": "Bootstrap", "modeClasses": [] },
 		]
 	},
 	{
 		title: "Mini Project Showcase",
 		description: "Discover a collection of creative mini-projects showcasing HTML, CSS, and JavaScript skills.",
-		image: "../../assets/mini-project-showcase.png",
+		image: "../../assets/project-images/mini-project-showcase.png",
 		code: "https://github.com/Daniel-Ho986/mini-project-showcase",
 		site: "https://daniel-ho986-mini-project-showcase.netlify.app/",
 		techStack: [
--- src/components/SkillPaths.ts
+++ src/components/SkillPaths.ts
@@ -1,29 +1,40 @@
 export const languages = [
-	{ path: "html5.svg", alt: "HTML5", modeClasses: [] },
-	{ path: "css3.svg", alt: "CSS3", modeClasses: [] },
-	{ path: "javascript.svg", alt: "JavaScript", modeClasses: [] },
-	{ path: "typescript.svg", alt: "TypeScript", modeClasses: [] },
-	{ path: "nodejs.svg", alt: "Node.js", modeClasses: [] },
-	{ path: "python.svg", alt: "Python", modeClasses: [] },
-	{ path: "git.svg", alt: "Git", modeClasses: [] },
-	{ path: "firebase.svg", alt: "Firebase", modeClasses: [] },
+    { path: "languages/html5.png", alt: "HTML5", modeClasses: [] },
+    { path: "languages/css3.png", alt: "CSS3", modeClasses: [] },
+    { path: "languages/js.svg", alt: "JavaScript", modeClasses: [] },
+    { path: "languages/typescript.svg", alt: "TypeScript", modeClasses: [] },
+    { path: "languages/python.png", alt: "Python", modeClasses: [] },
 ];
 
 export const libraries = [
-  	{ path: "reactjs.svg", alt: "React.js", modeClasses: [] },
-    { path: "express.svg", alt: "Express.js", modeClasses: ["light-mode-only"] },
-  	{ path: "express-dark.svg", alt: "Express.js", modeClasses: ["dark-mode-only"] },
-  	{ path: "nextjs.svg", alt: "Next.js", modeClasses: ["light-mode-only"] },
-	{ path: "nextjs-dark.svg", alt: "Next.js", modeClasses: ["dark-mode-only"] },
-	{ path: "astro.svg", alt: "Astro", modeClasses: ["light-mode-only"] },
-	{ path: "astro-dark.svg", alt: "Astro", modeClasses: ["dark-mode-only"] },
+    { path: "libraries/reactjs.png", alt: "React.js", modeClasses: [] },
+    { path: "libraries/nextjs.png", alt: "Next.js", modeClasses: ["light-mode-only"] },
+    { path: "libraries/nextjs-dark.png", alt: "Next.js", modeClasses: ["dark-mode-only"] },
+	{ path: "libraries/express.svg", alt: "Express.js", modeClasses: ["light-mode-only"] },
+    { path: "libraries/express-dark.svg", alt: "Express.js", modeClasses: ["dark-mode-only"] },
+    { path: "libraries/astro.png", alt: "Astro", modeClasses: ["light-mode-only"] },
+    { path: "libraries/astro-dark.png", alt: "Astro", modeClasses: ["dark-mode-only"] },
+    { path: "libraries/nodejs.png", alt: "Node.js", modeClasses: [] },
 ];
 
 export const databases = [
-  	{ path: "mongoDB.svg", alt: "MongoDB", modeClasses: [] },
+    { path: "databases/mongodb.png", alt: "MongoDB", modeClasses: [] },
+    { path: "databases/mongoose.png", alt: "Mongoose", modeClasses: [] },
+    { path: "databases/postgresql.png", alt: "PostgreSQL", modeClasses: [] },
 ];
 
 export const others = [
-	{ path: "cypress-icon.svg", alt: "Cypress", modeClasses: [] },
-	{ path: "playwright.svg", alt: "Playwright", modeClasses: [] },
+    { path: "others/firebase.svg", alt: "Firebase", modeClasses: [] },
+    { path: "others/cloudinary.png", alt: "Cloudinary", modeClasses: [] },
+	{ path: "others/bootstrap5.png", alt: "Bootstrap", modeClasses: ["light-mode-only"] },
+    { path: "others/bootstrap5-dark.png", alt: "Bootstrap", modeClasses: ["dark-mode-only"] },
+    // { path: "others/netlify.png", alt: "Netlify", modeClasses: [] },
+	// { path: "others/render.png", alt: "Render", modeClasses: ["light-mode-only"] },
+    // { path: "others/render-dark.png", alt: "Render", modeClasses: ["dark-mode-only"] },
+    { path: "others/cypress.png", alt: "Cypress", modeClasses: ["light-mode-only"] },
+    { path: "others/cypress-dark.png", alt: "Cypress", modeClasses: ["dark-mode-only"] },
+	{ path: "others/figma.svg", alt: "Figma", modeClasses: [] },
+    { path: "others/git.png", alt: "Git", modeClasses: [] },
+    { path: "others/postman.svg", alt: "Postman", modeClasses: [] },
+	{ path: "others/playwright.png", alt: "Playwright", modeClasses: [] },
 ];
